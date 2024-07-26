8b0ac7fd348cc4acad0bd9b6bcb81fa9931a9f2c
Wenz-jam <wjj20040520@163.com>
2024-07-26 23:12:15 +0800

    Fix the issue where ExpandedMacro inserts too many FunctionActions into entries. 修复ExpandedMacro向entries中插入过多FunctionAction的问题

--- src/main/java/top/mcfpp/mod/debugger/mixin/ExpandedMacroMixin.java
+++ src/main/java/top/mcfpp/mod/debugger/mixin/ExpandedMacroMixin.java
@@ -1,41 +1,29 @@
 package top.mcfpp.mod.debugger.mixin;
 
-import com.mojang.brigadier.CommandDispatcher;
-import net.minecraft.command.CommandExecutionContext;
 import net.minecraft.command.SourcedCommandAction;
-import net.minecraft.nbt.NbtCompound;
 import net.minecraft.server.command.AbstractServerCommandSource;
 import net.minecraft.server.function.ExpandedMacro;
-import net.minecraft.server.function.Procedure;
-import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import top.mcfpp.mod.debugger.command.FunctionInAction;
 import top.mcfpp.mod.debugger.command.FunctionOutAction;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Mixin(ExpandedMacro.class)
 public class ExpandedMacroMixin<T extends AbstractServerCommandSource<T>> {
 
-    @Unique private NbtCompound arguments = null;
-
     @Shadow @Final private List<SourcedCommandAction<T>> entries;
 
-    @Inject(method = "withMacroReplaced", at = @At("HEAD"))
-    private void OnWithMacroReplaced(@Nullable NbtCompound arguments, CommandDispatcher<T> dispatcher, CallbackInfoReturnable<Procedure<T>> cir){;
-
+    @Inject(method = "<init>", at = @At("TAIL"))
+    private void onInit(CallbackInfo ci) {
         final var THIS = (ExpandedMacro<T>) (Object) this;
-
         entries.addFirst(new FunctionInAction<>(THIS));
         entries.add(new FunctionOutAction<>(THIS));
-
     }
 
 }
